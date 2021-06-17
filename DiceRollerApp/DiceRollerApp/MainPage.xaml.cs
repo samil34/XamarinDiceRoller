d6c93c557b802b2052b0eb58f9956957a66c39b6
Samil Akpinar <samilakpinar6@gmail.com>
2021-06-17 16:55:44 +0300

    Add project files.

---
+++ DiceRollerApp/DiceRollerApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DiceRollerApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int randomValue;
+
+        Label label;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            image.Source = ImageSource.FromResource("DiceRollerApp.images.diegeneral.png");
+            label = (Label)FindByName("number");
+
+            label.Text = "Random Number";
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            randomValue = new Random().Next(1, 7);
+            image.Source = ImageSource.FromResource("DiceRollerApp.images.dice" + randomValue + ".png");
+            label.Text = " " + randomValue;
+        }
+    }
+}
